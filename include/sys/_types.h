5dcdd4adbbe9dac26df3533ddd37a4e922e70793
Andrew Apperley <mail@andrewapperley.ca>
2023-10-21 22:05:12 -0400

    Brought over pthreads from https://github.com/KallistiOS/KallistiOS/pull/283

--- include/sys/_types.h
+++ include/sys/_types.h
@@ -14,6 +14,7 @@ __BEGIN_DECLS
 
 #include <sys/lock.h>
 #include <newlib.h>
+#include <sys/_pthreadtypes.h>
 
 // This part copied from newlib's sys/_types.h.
 #ifndef __off_t_defined
@@ -199,9 +200,6 @@ typedef _CLOCK_T_   __clock_t;
 
 /* This is for old KOS source compatability. */
 #include <arch/types.h>
-
-/* Include stuff to make pthreads work as well. */
-#include <sys/_pthread.h>
 
 #if __GNUC_MINOR__ > 95 || __GNUC__ >= 3
 typedef __builtin_va_list   __va_list;
