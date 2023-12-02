82bd144c4d79f135b2b82074c799f399c95b1ae3
Andrew Apperley <mail@andrewapperley.ca>
2023-12-02 14:40:47 -0500

    Updated hello example

--- examples/dreamcast/hello/hello.m
+++ examples/dreamcast/hello/hello.m
@@ -1,32 +1,29 @@
-#include <stdio.h>
 #import <Foundation/Foundation.h>
 
-@interface Person: NSObject
+@interface Dreamcast: NSObject {
+  NSTimer *timer;
+}
 - (void)test;
 @end
 
-@implementation Person
-- (void)test {
-  NSLog(@"hello dc");
-  NSString *json = @"\
-{\
-  \"random_double\": 123.456789012254,\
-}";
-  NSError *e;
-  NSData *data = [json dataUsingEncoding: NSUTF8StringEncoding];
-  NSDictionary *obj = [NSJSONSerialization JSONObjectWithData: data options: 0 error: &e];
-  NSLog(@"%f", [obj valueForKey: @"random_double"]);
-}
+@implementation Dreamcast
+  - (void)test {
+    timer = [NSTimer scheduledTimerWithTimeInterval: 1 target: self selector: @selector(printSomething:) userInfo: nil repeats: YES];
+  }
+  - (void)printSomething:(NSTimer *)timer {
+    NSLog(@"Wow! Is that Objective-C concurrency and NSLog working on the Dreamcast!");
+  }
 @end
 
 int main(int argc, char **argv, char **envp) {
+    gdb_init();
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];     
-    NSLog(@"start");
-    Person *person = [[Person alloc] init];
-    [person test];
-    NSLog(@"end");
-    NSLog(@"this came from nslog");
+
+    Dreamcast *dc = [[Dreamcast alloc] init];
+    [dc test];
+    [[NSRunLoop currentRunLoop] run];
+    [pool release];
+
     fflush(stdout);
-    [pool drain];
     return 0;
 }
