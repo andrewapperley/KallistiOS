c39bc95a35c7257d4ad5a9b083ed1b8e6be10df3
Andrew Apperley <mail@andrewapperley.ca>
2023-12-23 06:23:21 -0500

    Updated example with blank example

--- examples/dreamcast/hello/hello.m
+++ examples/dreamcast/hello/hello.m
@@ -1,29 +1,11 @@
-#import <Foundation/Foundation.h>
+#include	<Foundation/Foundation.h>
 
-@interface Dreamcast: NSObject {
-  NSTimer *timer;
-}
-- (void)test;
-@end
+int main(int argc, char *argv[], char **env)
+{
+  NSAutoreleasePool	*pool = [[NSAutoreleasePool alloc] init];
+  
 
-@implementation Dreamcast
-  - (void)test {
-    timer = [NSTimer scheduledTimerWithTimeInterval: 1 target: self selector: @selector(printSomething:) userInfo: nil repeats: YES];
-  }
-  - (void)printSomething:(NSTimer *)timer {
-    NSLog(@"Wow! Is that Objective-C concurrency and NSLog working on the Dreamcast!");
-  }
-@end
-
-int main(int argc, char **argv, char **envp) {
-    gdb_init();
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];     
-
-    Dreamcast *dc = [[Dreamcast alloc] init];
-    [dc test];
-    [[NSRunLoop currentRunLoop] run];
-    [pool release];
-
-    fflush(stdout);
-    return 0;
+  [[NSRunLoop currentRunLoop] run];
+  [pool release];
+  return 0;
 }
